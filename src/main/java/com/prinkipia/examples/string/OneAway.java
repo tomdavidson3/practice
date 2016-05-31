b6cababbd9527e51a6cedaa116bcc595101d8c8f
tomdavidson3 <tom.davidson3@gmail.com>
2016-05-31 16:38:04 -0700

    Examples of coding interview questions

---
+++ src/main/java/com/prinkipia/examples/string/OneAway.java
@@ -0,0 +1,51 @@
+package com.prinkipia.examples.string;
+
+public class OneAway {
+    static boolean numEdits(String source, String target) {
+        if (source.equals(target)) {
+            return true;
+        }
+
+        int diff = target.length() - source.length();
+        if (diff == 0) {
+            // if strings are same length then check there is only one difference in characters
+            return numDifferences(source, target) == 1;
+        }
+        if (diff == 1) {
+            return checkInsert(source, target);
+        }
+        if (diff == -1) {
+            return checkInsert(target, source);
+        }
+        return false;
+    }
+
+    private static boolean checkInsert(final String source, final String target) {
+        int j = 0;
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) == target.charAt(j)) {
+                j++;
+            }
+            j++;
+        }
+        return j == 1;
+    }
+
+    private static int numDifferences(final String source, final String target) {
+        int count = 0;
+        for (int i = 0; i < source.length(); i++) {
+            if (source.charAt(i) != target.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numEdits("pale", "pale"));
+        System.out.println(numEdits("pales", "pale"));
+        System.out.println(numEdits("pale", "bale"));
+        System.out.println(numEdits("pale", "bake"));
+        System.out.println(numEdits("pale", "ba"));
+    }
+}
